68d03bc6200126fadef5bc18e8c2706d7defccab
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-16 11:41:33 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Zawadi Opoti";
+ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
